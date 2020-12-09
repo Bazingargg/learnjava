73fd335ef2565aef85823998fbf69a128c55e6d7
Bazingargg <szm1997@icloud.com>
2020-12-09 23:33:29 +0800

    i fied a bug

--- proj0/NBody.java
+++ proj0/NBody.java
@@ -57,15 +57,15 @@ public class NBody {
 			StdDraw.pause(10);
 			time = time + dt;
 		}
-		StdOUt.printf("%d\n", planets.length);
-		StdOUt.printf("%.2e\n", radius);
+		StdOut.printf("%d\n", planets.length);
+		StdOut.printf("%.2e\n", radius);
 		for (int i = 0; i < planets.length; i++) {
-			StdOUt.printf("%11.4e %11.4e %11.4e %11.4e %11.4e %12s\n",
+			StdOut.printf("%11.4e %11.4e %11.4e %11.4e %11.4e %12s\n",
 							planets[i].xxPos, planets[i].yyPos, planets[i].xxVel, 
 							planets[i].yyVel, planets[i].mass, planets[i].imgFileName);
 		}
 	}
-		
+
 }
 
 
