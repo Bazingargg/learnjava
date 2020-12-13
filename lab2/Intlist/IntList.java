97d9ac8b46dd16bf79dd8d22d7baa29fedd3ea26
Bazingargg <szm1997@icloud.com>
2020-12-13 16:12:40 +0800

    need to return B

--- lab2/Intlist/IntList.java
+++ lab2/Intlist/IntList.java
@@ -83,7 +83,7 @@ public class IntList {
     public static IntList dcatenate(IntList A, IntList B) {
         //TODO:  fill in method
         if (A == null) {
-            return null;
+            return B;
         }
         IntList pta = A;
         while (A.rest != null) {
@@ -100,7 +100,7 @@ public class IntList {
     public static IntList catenate(IntList A, IntList B) {
         //TODO:  fill in method
         if (A == null) {
-            return null;
+            return B;
         }
         IntList q = new IntList(A.first, null);
         IntList pq = q;
