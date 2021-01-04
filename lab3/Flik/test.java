45f44907b76124356241c6642bbd4d057eb1d385
Bazingargg <szm1997@icloud.com>
2021-01-05 00:06:47 +0800

    add a useless test.java

---
+++ lab3/Flik/test.java
@@ -0,0 +1,12 @@
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class test {
+
+    @Test
+    public void testFlik() {
+        int a = 129;
+        int b = 129;
+        assertTrue(Flik.isSameNumber(a, b));
+    }
+}
